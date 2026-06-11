a5e350f6d11b887af0791c35902bc7abf5aba831
likitha <likithasrit@gmail.com>
2026-06-11 21:01:48 +0530

    Fix accidental cd in server

--- server/index.js
+++ server/index.js
@@ -28,4 +28,4 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => res.send('VitaeX backend running ✅'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));cd
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
